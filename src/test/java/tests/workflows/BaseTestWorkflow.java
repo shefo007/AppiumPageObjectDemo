609e77cf44524312105ed4e038581c20a56ded77
Sherif Abdallah <sheriftawaptawap@gmail.com>
2023-07-16 02:05:42 +0300

    edit 2

--- src/test/java/tests/workflows/BaseTestWorkflow.java
+++ src/test/java/tests/workflows/BaseTestWorkflow.java
@@ -24,7 +24,7 @@ public class BaseTestWorkflow {
         options.setPlatformName("Android");
         options.setPlatformVersion("11.0");
         options.setDeviceName("emulator-5554");
-        options.setApp("/apps/ToDo.apk");
+        options.setApp("https://github.com/shefo007/AppiumPageObjectDemo/blob/5cee44b5f77ab2216dc2176ff4b789e82a8fd48b/apps/ToDo.apk");
 
         driver = new AndroidDriver(new URI(DEFAULT_URL).toURL(), options);
     }
